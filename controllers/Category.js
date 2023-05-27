0c0abcd26412a6d566d91c2c2a804a6408e8db2d
NOTEBOOKDELL\Dell <aribrenman@hotmail.com>
2023-05-26 23:21:33 -0300

    Escribe en la base

--- controllers/Category.js
+++ controllers/Category.js
@@ -1,5 +1,5 @@
 
-
+const table = 'Category';
  const admin = require('firebase-admin')
 
  var serviceAccount = require("../gastosdiarios-e2f45-firebase-adminsdk-jesw0-d61e0f952c.json");
@@ -8,24 +8,49 @@
     databaseURL: "https://gastosdiarios-e2f45-default-rtdb.firebaseio.com"
   });
  const db = admin.database();
-function add(req, res){
- 
-   
+
+async function add(req, res,){
+  const date = new Date();
+  
     const category = {
-        id: '',
+        key: '',
         name: req.body.name,
-        image: req.body.image
+        image: req.body.image,
+        createdDate: date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate(),
+        modifiedDate: ''
     }
-   const id = db.ref('Category').push(category);
-   console.log(id);
+    /*
+category.key = res;
+    req.body.key = res;
+    db.ref(table).update(category.key, {
+        key: category.key,
+        name: category.name,
+        image: category.image,
+        createdDate: category.createdDate,
+        modifiedDate: date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate()
+    })
+    */
+   const key = db.ref(table).push(category)
+  
+
+   
+  
 // Devolvemos una respuesta en JSON
-    res.status(200).send({
-        menssage: 'Esta ruta es de prueba en mi api restful con mongo y node'
+    res.status(200). send({
+        menssage: 'Se genero la key ' + key,
+        
     });
-}
+    
+}  
+  
 function edit(req, res){
  
-    // Devolvemos una respuesta en JSON
+     db.ref(table).update(req.key, {
+                                    key: req.key,
+                                    name: req.name,
+                                    image: req.image
+
+                                     })
         res.status(200).send({
             menssage: 'Esta ruta es de prueba en mi api restful con mongo y node'
         });
@@ -45,7 +70,7 @@ function getAll(req, res){
         });
     }
 function getById(req, res){
-
+    console.log('paso el get');
     // Devolvemos una respuesta en JSON
         res.status(200).send({
             menssage: 'Esta ruta es de prueba en mi api restful con mongo y node'
